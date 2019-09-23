1d32a6be43f320c1ab29819ab3a795763ef3eaa8
Atrus619 <aj.gray619@gmail.com>
2019-09-23 12:38:05 -0500

    Move indeed extract functions to appropriate file and assign Jen TODOs for next steps

---
+++ src/data/make_dataset.py
@@ -0,0 +1,11 @@
+from src.utils.scraping_indeed import *
+
+
+def make_dataset():
+    """
+    Accepts arguments describing the search parameters (probably a list of tuples of arguments for build_url)
+    Pulls information from indeed, parses it into meaningful components, and inserts this information to mongodb
+    :return: Nothing, adds extracted information to mongodb
+    """
+    # TODO: Jen, build this function and its dependencies!
+    pass
