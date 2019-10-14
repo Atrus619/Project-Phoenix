655e3b2f4a713ff53707c01fd83c5692c21cb976
Atrus619 <aj.gray619@gmail.com>
2019-10-13 21:17:47 -0500

    Add user agent cycling functionality

---
+++ src/constants.py
@@ -0,0 +1,19 @@
+class Constants:
+    # List of IP Vanish servers: https://account.ipvanish.com/index.php?t=Server%20List
+    ipvanish_base_links = [
+        ('iad-a01.ipvanish.com', 70),
+        ('jnb-c01.ipvanish.com', 7)
+    ]
+
+    # List of user agents: https://developers.whatismybrowser.com/useragents/explore/
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
+        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)',
+        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+    ]
--- src/scraping/scraping_monster.py
+++ src/scraping/scraping_monster.py
@@ -87,14 +87,15 @@ def extract_job_link_from_result(soup):
     return links
 
 
-def extract_description_from_link(link):
+def extract_description_from_link(link, user_agent):
     """
     Retrieves the full job description from an indeed job posting link
     :param link: indeed job posting link (excludes the indeed.com part)
+    :param user_agent: User Agent to be used with the request
     :return: text of full job description
     """
     url = link
-    page = requests.get(url)
+    page = requests.get(url, headers={'User-Agent': user_agent})
     soup = BeautifulSoup(page.text, "html.parser")
     raw_descr = soup.find_all(name='div', attrs={'id': 'JobDescription'})
 
