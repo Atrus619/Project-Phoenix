0fdb499633b12455cf1d9978929d6cb41f0ddaeb
Atrus619 <aj.gray619@gmail.com>
2019-10-21 11:29:47 -0500

    Set up files for additional sources (all blank) and continue handling issues with web scraper in action

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,13 @@
 class Constants:
     # List of IP Vanish servers: https://account.ipvanish.com/index.php?t=Server%20List
+    # TODO: Jen: Expand list later
     ipvanish_base_links = [
         ('iad-a01.ipvanish.com', 70),
         ('jnb-c01.ipvanish.com', 7)
     ]
 
     # List of user agents: https://developers.whatismybrowser.com/useragents/explore/
+    # TODO: Jen: Expand list later
     user_agents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
