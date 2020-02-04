3c83e9e3b46354749bcdcd9697540f1ea9538844
Atrus619 <aj.gray619@gmail.com>
2020-02-03 22:44:48 -0600

    Progress made on putting chatbot on web client

--- app/index.js
+++ app/index.js
@@ -1,21 +1,35 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var user_io = require('socket.io')(http);
+
+var chatbot_server = require('net');
+var chatbot_client = new chatbot_server.Socket();
+chatbot_client.connect(8765, '127.0.0.1', function(){
+  console.log('listening for chatbot inputs on *:8765')
+});
+chatbot_client.on('msg', function(msg){
+  console.log('Bot:', msg)
+})
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+user_io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    console.log('User:', msg)
+    user_io.emit('chat message', msg);
   });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening for user inputs on *:3000');
 });
+
+// Trying these links:
+// https://stackoverflow.com/questions/39184455/connect-js-client-with-python-server
+// https://ianhinsdale.com/post/communicating-between-nodejs-and-python/
--- src/classes/ChatBot.py
+++ src/classes/ChatBot.py
@@ -11,59 +11,49 @@ class ChatBot:
         self.policy = policy
         self.conversation_history = ConversationHistory()
 
-    def interact(self):
-        opening_msg = self.policy.get_opening_msg()
-        self.conversation_history.add_bot_msg(raw_text=opening_msg)
+    def console_interact(self):
+        opening_msg = self.update_history_and_generate_opening_msg()
         print(opening_msg)
 
         while True:
             raw_text = self.seek_input_from_user()
-
-            # Parse and update based on user input
             parsed_user_msg = self.interpreter.parse_user_msg(raw_text=raw_text)
-            self.conversation_history.add_parsed_user_msg(*parsed_user_msg)
-
-            # Reply
-            reply = self.policy.get_reply(conversation_history=self.conversation_history)
-            self.conversation_history.add_bot_msg(raw_text=reply)
+            reply = self.update_history_and_generate_reply(parsed_user_msg=parsed_user_msg)
             print(reply)
 
-            # Check if exit condition reached and break
-            if self.conversation_history.user_msgs[-1].classified_intent == cfg.valid_intents['end_of_conversation']:
+            if self.exit_conversation():
                 break
 
             # Check if additional information was sought and continue asking until all information determined for request
             if self.policy.is_seeking_additional_info():
                 additional_info_conv_history = ConversationHistory()
+                while self.policy.is_seeking_additional_info():
+                    raw_text = self.seek_input_from_user()
+                    reply = self.update_history_and_get_more_information(input_msg=raw_text, original_parsed_user_msg=parsed_user_msg,
+                                                                         additional_info_conv_history=additional_info_conv_history)
+                    print(reply)
 
-            while self.policy.is_seeking_additional_info():
-                raw_text = self.seek_input_from_user()
+    async def served_interact(self, websocket, path):
+        opening_msg = self.update_history_and_generate_opening_msg()
+        await websocket.send(opening_msg)
 
-                # Parse and updated based on user input, same as earlier, but with the follow up variant of the routine
-                parsed_user_follow_up_msg = self.interpreter.parse_user_msg_follow_up(raw_text=raw_text, missing_entity=self.policy.missing_entity)
-                self.conversation_history.add_parsed_user_msg(*parsed_user_follow_up_msg)
+        while True:
+            raw_text = await websocket.recv()
+            parsed_user_msg = self.interpreter.parse_user_msg(raw_text=raw_text)
+            reply = self.update_history_and_generate_reply(parsed_user_msg=parsed_user_msg)
+            await websocket.send(reply)
 
-                # Reply: Either way, reset missing entity. If additional information is sought after accepting, it will get set to missing again before re-entering
-                # while loop.
-                missing_entity_letter = self.policy.missing_entity
-                self.policy.reset_missing_entity()
+            if self.exit_conversation():
+                break
 
-                # If accept, then call routine to generate shallow conversation_history object with updated query to get the actual reply if everything is satisfied.
-                # Otherwise, re-enter while loop at the top to append to this conversation_history object.
-                if self.conversation_history.get_latest_msg().classified_intent == 'Acceptance':
-                    additional_info_conv_history.add_parsed_user_msg(*parsed_user_msg)
-
-                    additional_entity_text = self.conversation_history.get_latest_msg().recognized_entities[missing_entity_letter][-1]
-                    additional_info_conv_history.update_latest_recognized_entities(entity_letter=missing_entity_letter,
-                                                                                   additional_entity_text=additional_entity_text)
-                    reply = self.policy.get_reply(conversation_history=additional_info_conv_history)
-                else:
-                    reply = self.policy.get_reply(conversation_history=self.conversation_history)
-
-                # Whether accept or reject, need to update reply and continue. If additional information is needed, policy missing entity will get reset and while loop
-                # will be re-entered.
-                self.conversation_history.add_bot_msg(raw_text=reply)
-                print(reply)
+            # Check if additional information was sought and continue asking until all information determined for request
+            if self.policy.is_seeking_additional_info():
+                additional_info_conv_history = ConversationHistory()
+                while self.policy.is_seeking_additional_info():
+                    raw_text = await websocket.recv()
+                    reply = self.update_history_and_get_more_information(input_msg=raw_text, original_parsed_user_msg=parsed_user_msg,
+                                                                         additional_info_conv_history=additional_info_conv_history)
+                    await websocket.send(reply)
 
     @staticmethod
     def seek_input_from_user():
@@ -72,3 +62,46 @@ class ChatBot:
             print('Please enter in a non-empty value.')
             raw_text = input('>>> ')
         return raw_text
+
+    def update_history_and_generate_opening_msg(self):
+        opening_msg = self.policy.get_opening_msg()
+        self.conversation_history.add_bot_msg(raw_text=opening_msg)
+        return opening_msg
+
+    def update_history_and_generate_reply(self, parsed_user_msg):
+        # Update history
+        self.conversation_history.add_parsed_user_msg(*parsed_user_msg)
+
+        # Reply
+        reply = self.policy.get_reply(conversation_history=self.conversation_history)
+        self.conversation_history.add_bot_msg(raw_text=reply)
+        return reply
+
+    def exit_conversation(self):
+        return self.conversation_history.user_msgs[-1].classified_intent == cfg.valid_intents['end_of_conversation']
+
+    def update_history_and_get_more_information(self, input_msg, original_parsed_user_msg, additional_info_conv_history):
+        # Parse and updated based on user input, same as earlier, but with the follow up variant of the routine
+        parsed_user_follow_up_msg = self.interpreter.parse_user_msg_follow_up(raw_text=input_msg, missing_entity=self.policy.missing_entity)
+        self.conversation_history.add_parsed_user_msg(*parsed_user_follow_up_msg)
+
+        # Reply: Either way, reset missing entity. If additional information is sought after accepting, it will get set to missing again before re-entering while loop.
+        missing_entity_letter = self.policy.missing_entity
+        self.policy.reset_missing_entity()
+
+        # If accept, then call routine to generate shallow conversation_history object with updated query to get the actual reply if everything is satisfied.
+        # Otherwise, re-enter while loop at the top to append to this conversation_history object.
+        if self.conversation_history.get_latest_msg().classified_intent == 'Acceptance':
+            additional_info_conv_history.add_parsed_user_msg(*original_parsed_user_msg)
+
+            additional_entity_text = self.conversation_history.get_latest_msg().recognized_entities[missing_entity_letter][-1]
+            additional_info_conv_history.update_latest_recognized_entities(entity_letter=missing_entity_letter,
+                                                                           additional_entity_text=additional_entity_text)
+            reply = self.policy.get_reply(conversation_history=additional_info_conv_history)
+        else:
+            reply = self.policy.get_reply(conversation_history=self.conversation_history)
+
+        # Whether accept or reject, need to update reply and continue. If additional information is needed, policy missing entity will get reset and while loop
+        # will be re-entered.
+        self.conversation_history.add_bot_msg(raw_text=reply)
+        return reply
